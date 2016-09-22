72f50b2226dffd2315c5ad1f7172b00c846fda8d
maria <maria.braun@buttinette.de>
2016-09-22 16:22:38 +0200

    berichtsheft: Datenbankconnection funktioniert

--- getData.php
+++ getData.php
@@ -1,11 +1,10 @@
 <?php
 
 include "connect.php";
-
 try {
     
    $key = $_POST['loadkey'];
-	echo $redis->get("Maria Braun");
+	echo $redis->get("test");
 	
 } catch (Exception $e) {
     echo $e;
